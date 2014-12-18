66ca26f333ef080af1de012e5e397ecccc9930b8
manoja328 <manoja328@gmail.com>
2014-12-18 15:13:08 +0545

    modify bresham using my own methods good but slow

--- main.c
+++ main.c
@@ -1,113 +1,67 @@
 #include "lcd.h"
-
-#define PERIOD 5000000
-#define PER_COUNTS ((PERIOD / 100) - 1)  // don't edit this!
-#define PER_REMAINDER (PERIOD - (PER_COUNTS * 100))  // don't edit this!
-unsigned long pcount=0;    // used in interrupt to count PER_COUNTS
-unsigned char calc_bit=0;
-unsigned long RPM_Value=0;
-
-
-void interrupt isr()
+unsigned char RPM_Value=0;
+unsigned int pcount=0;    
+void delay_1s()
 {
 
-    if(!pcount)
-    {
+	while(pcount>0)
+	{
 
-        TMR1ON = 0;    // Stop the timer	
-        calc_bit=1;
-        pcount = (PER_COUNTS+1);    // how many delays to make total
-        TMR0 -=(PER_REMAINDER-3);  // first delay will be ==remainder
-    }
-    // else make a normal delay
+		if (pcount % 4 ==0)
+			TMR0 -=157;
+		else
+			TMR0 -=156;
+		while(!TMR0IF) {};
+		TMR0IF=0;   //the flag shoud be reset before leaving                                
+		pcount--;
+	}
 
-    else
-    {
-
-
-		
-        TMR0 -= (100-2);       // make another 100 tick delay
-    }
-    pcount--;
-    //-----------------------------------------------------
-    // clear the TMR0 overflow flag and exit
-    T0IF = 0;
+	pcount = 2000;
+   
 }
-
-unsigned char delay;
-void delay_10us(unsigned char t)
-// Provides t * 10 usecs of delay.
-// Max of t is 255 which corresponds to 2550 usecs 
-{
-delay = t;
-/*  Below is an assembly language fragment to 
- *  create a 10 microsecond delay.  Note that in 
- *  this fragment the C global variable "delay" 
- *  is written as "_delay".
- */
-#asm
-DELAY_10US_1: 
-   CLRWDT 
-   NOP
-   NOP
-   NOP
-   NOP
-   NOP
-   NOP 
-   DECFSZ  _delay,f 
-   GOTO    DELAY_10US_1
-#endasm 
-} 
-
-
-
 
 void main(void)
 {
-    TRISB = 0b00000000;
-
-
+    TRISB=0X00;
     initLCD();
     ADCON1 = 0x06;  // Disable Analog functions
     T1CON = 0b00001110; // TMR1 as 16-bit counter
+    TMR1L = 0;
+    TMR1H = 0;
 
 
+    T0CS=0;	//internal instruction clock cycle
+    PSA=0;  //selecting tmro prescaler
+    PS2=1;	//prescaler defining 1:128
+    PS1=0;
+    PS0=0;
+    TMR0IF=0;	//disabling tmr0 interrupt
 
 
-   	LCD_goto(1,0);
+    LCD_goto(1,0);
     __delay_ms(1);
     lcd_puts ("  Tachometer  ");
     __delay_ms(1);
     LCD_goto(2,10);
     __delay_ms(1);
-    lcd_puts ("RPM");
+    lcd_puts ("RPS");
     __delay_ms(1);
 
-	TMR0 =0;
-    OPTION = 0b00001000;    // TMR0 on, 1:1 prescale
-    INTCON = 0b10100000;  // GIE on, T0IE on (turn interrupt on)
 
     while(1)
     {
         
-        if (calc_bit==1)
-        {
 
-            RPM_Value = (256*TMR1H + TMR1L) ;
-            LCD_goto(2,5);
-            LCD_num (RPM_Value);
-            TMR1L = 0;
-            TMR1H = 0;
-            calc_bit=0;
-            TMR1ON = 1;            
+        TMR1ON = 1;
+        delay_1s();
+        TMR1ON = 0;
 
-        }
+        RPM_Value = (256*TMR1H + TMR1L)  ;
+        TMR1L = 0;
+        TMR1H = 0;
+        LCD_goto(2,5);
+        LCD_num (RPM_Value/2);
 
     }
 
 }
-
-
-
-
-
