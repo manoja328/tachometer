e65de9c1f0a7afd4556ae5b27f714a9df1b5f74f
manoja328 <manoja328@gmail.com>
2014-12-18 15:43:17 +0545

    average and use own delay tmr0

--- main.c
+++ main.c
@@ -1,16 +1,11 @@
 #include "lcd.h"
-#define PERIOD 5000000/4
-#define PER_COUNTS ((PERIOD / 100) - 1)  // don't edit this!
-#define PER_REMAINDER (PERIOD - (PER_COUNTS * 100))  // don't edit this!
-unsigned long pcount=0;    // used in interrupt to count PER_COUNTS
-unsigned char calc_bit=0;
-unsigned char RPM_Value=0;
-int C;
-
+unsigned char RPM_Value=0,tt;
+unsigned int pcount=0;
+float C;
 
 struct sAve
 {
-    unsigned long blockAverage;
+    unsigned int blockAverage;
     unsigned char numSamples;
 };
 
@@ -20,65 +15,54 @@ void AddSampleToAverage(struct sAve *ave, unsigned char newSample)
     ave->numSamples++;
 }
 
-float average; 
-unsigned char GetAverage(struct sAve *ave)
+float average;
+float GetAverage(struct sAve *ave)
 {
     average =(ave->blockAverage*1.0)/(ave->numSamples);
     ave->blockAverage = 0;
     ave->numSamples = 0;
-    return  (unsigned char)average;
+    return  average;
 
 }
 
 
 
-void interrupt isr()
+
+void delay_1s()
 {
 
-    if(!pcount)
+    while(pcount>0)
     {
 
-        TMR1ON = 0;    // Stop the timer
-        calc_bit=1;
-        pcount = (PER_COUNTS+1);    // how many delays to make total
-        TMR0 -=(PER_REMAINDER-3);  // first delay will be ==remainder
+        if (pcount % 8 ==0)
+            TMR0 -=79;
+        else
+            TMR0 -=78;
+        while(!TMR0IF) {};
+        TMR0IF=0;   //the flag shoud be reset before leaving
+        pcount--;
     }
-    // else make a normal delay
 
-    else
-    {
+    pcount = 1000;
 
-
-
-        TMR0 -= (100-2);       // make another 100 tick delay
-    }
-    pcount--;
-    //-----------------------------------------------------
-    // clear the TMR0 overflow flag and exit
-    T0IF = 0;
 }
-
-
-
-
-
 
 void main(void)
 {
-
-    struct sAve myavg;
-
-    myavg.blockAverage=0;
-    myavg.numSamples=0;
-
-TRISB=0X00;
-
-
+    TRISB=0X00;
     initLCD();
     ADCON1 = 0x06;  // Disable Analog functions
     T1CON = 0b00001110; // TMR1 as 16-bit counter
 
 
+    T0CS=0;	//internal instruction clock cycle
+    PSA=0;  //selecting tmro prescaler
+    PS2=1;	//prescaler defining 1:32
+    PS1=0;
+    PS0=0;
+    TMR0IF=0;	//disabling tmr0 interrupt
+	TMR0IE=0;
+TMR0 -=78;
 
 
     LCD_goto(1,0);
@@ -87,60 +71,45 @@ TRISB=0X00;
     __delay_ms(1);
     LCD_goto(2,10);
     __delay_ms(1);
-    lcd_puts ("RPM");
+    lcd_puts ("RPS");
     __delay_ms(1);
 
-    TMR0 =0;
-    OPTION = 0b00001000;    // TMR0 on, 1:1 prescale
-    INTCON = 0b10100000;  // GIE on, T0IE on (turn interrupt on)
+
+    struct sAve myavg;
+
+    myavg.blockAverage=0;
+    myavg.numSamples=0;
+
+
 
     while(1)
     {
+        TMR1L = 0;
+        TMR1H = 0;
+        TMR1ON = 1;
+        delay_1s();
+        TMR1ON = 0;
 
-        if (calc_bit==1)
+        RPM_Value = (256*TMR1H + TMR1L);
+
+        AddSampleToAverage(&myavg,RPM_Value);
+
+        if(myavg.numSamples == 2)
         {
 
-            RPM_Value = (256*TMR1H + TMR1L) ;
-
-
-            AddSampleToAverage(&myavg,RPM_Value);
-
-            if(myavg.numSamples == 4)
-            {
-
-                C= GetAverage(&myavg);
-				C=C*60*4;
-               LCD_goto(2,5);
-                LCD_num (C);
- 
-
-            }
-
-
-
-            TMR1L = 0;
-            TMR1H = 0;
-            calc_bit=0;
-            TMR1ON = 1;
+            C= GetAverage(&myavg);
+            C= C * 2;
+			
+            LCD_goto(2,1);
+            LCD_num (C);
+    		lcd_puts (".");
+//			tt= (int)(C * 10) % 10;
+			
+//			LCD_num ( tt );	
 
         }
+
 
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
